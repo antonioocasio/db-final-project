8c513bdc613efbb08edc402f11d81270b35ebdeb
antonioocasio <46731816+antonioocasio@users.noreply.github.com>
2019-12-09 14:52:54 -0600

    Add files via upload

--- usernamesandpasswords.sql
+++ usernamesandpasswords.sql
@@ -1,24 +1,27 @@
-CREATE DATABASE usernamesandpasswords;
+-- CREATE DATABASE usernamesandpasswords;
 
 CREATE TABLE users(
-  userID INT NOT NULL,
+  userID INT NOT NULL AUTO_INCREMENT,
   firstName VARCHAR(35),
   lastName VARCHAR(35),
   phoneNumber INT(10),
   PRIMARY KEY(userID)
 );
 
+CREATE TABLE category(
+  categoryID INT AUTO_INCREMENT PRIMARY KEY,
+  categoryName CHAR(15)
+);
+
 CREATE TABLE credentials(
-  userID INT NOT NULL,
+  userID INT NOT NULL AUTO_INCREMENT,
   websiteName VARCHAR(25),
   username VARCHAR(35),
   password VARCHAR(50),
   dateCreated DATE,
-  PRIMARY KEY(userID)
+  categoryID INT,
+  PRIMARY KEY(userID),
+  FOREIGN KEY(userID) REFERENCES users(userID),
+  FOREIGN KEY(categoryID) REFERENCES category(categoryID)
 );
 
-CREATE TABLE category(
-  categoryID INT NOT NULL,
-  categoryName CHAR(15),
-  PRIMARY KEY(categoryID)
-);
